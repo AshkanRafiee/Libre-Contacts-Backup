3976449f26b302574a6331f8ea750bb3dc4f9e2e
Ashkan Rafiee <github.com.undermine023@passmail.com>
2026-09-21 07:49:23 +0000

    About: show the donate fragment in the donation row

--- app/src/main/java/com/ashkanrafiee/librecontactsbackup/AboutActivity.java
+++ app/src/main/java/com/ashkanrafiee/librecontactsbackup/AboutActivity.java
@@ -58,7 +58,7 @@ public class AboutActivity extends Activity {
         body.addView(info(getString(R.string.about_source_label), "github.com/AshkanRafiee/Libre-Contacts-Backup", v -> open("https://github.com/AshkanRafiee/Libre-Contacts-Backup")), margins(0, 0, 0, 8));
         body.addView(info(getString(R.string.about_website_label), "librecontactsbackup.ashkanrafiee.com", v -> open(APP_WEBSITE)), margins(0, 0, 0, 8));
         body.addView(info(getString(R.string.about_suggestions_label), "github.com/AshkanRafiee/Libre-Contacts-Backup/issues", v -> open(ISSUES_URL)), margins(0, 0, 0, 8));
-        body.addView(info(getString(R.string.about_donate_label), "librecontactsbackup.ashkanrafiee.com/donate", v -> open(DONATION_URL)), margins(0, 0, 0, 8));
+        body.addView(info(getString(R.string.about_donate_label), "librecontactsbackup.ashkanrafiee.com/#donate", v -> open(DONATION_URL)), margins(0, 0, 0, 8));
         body.addView(info(getString(R.string.about_privacy_label), getString(R.string.about_privacy_value), null), margins(0, 0, 0, d(20, 16)));
         String version = "1.0"; try { version = getPackageManager().getPackageInfo(getPackageName(), 0).versionName; } catch (Exception ignored) { }
         TextView footer = text(getString(R.string.about_footer, version), 11, textTertiary); footer.setGravity(Gravity.CENTER); body.addView(footer);
